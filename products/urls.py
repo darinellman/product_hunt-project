4fcd75a3d2bc3e13bb79e51f4a4f95739217d229
Darin <drnellman@gmail.com>
2019-11-25 10:57:05 +0200

    just got the product detail page wired up and now starting to create it

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('create', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='detail'),
 ]
